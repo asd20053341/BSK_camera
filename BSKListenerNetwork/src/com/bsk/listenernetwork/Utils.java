c8900e00f2f8b7f246561f98900d5d42251dc8b7
zhangbin <binzhang@bsktek.com>
2016-01-30 11:56:15 +0800

    提交AutoClear

--- BSKListenerNetwork/src/com/bsk/listenernetwork/Utils.java
+++
@@ -1,7 +0,0 @@
-package com.bsk.listenernetwork;
-
-import android.os.Environment;
-
-public class Utils {
-	public static final String URL = Environment.getExternalStorageDirectory() + "/" + "NetworkConnect.xml";
-}
--- root_access/gen/com/bsk/root_access/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.bsk.root_access;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
